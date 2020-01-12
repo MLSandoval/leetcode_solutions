b617b061fc4268b43ecdf2bd8ff59f307df6d807
mlsandoval <michaelsandoval8@gmail.com>
2020-01-11 23:49:51 -0800

    not tracking variable value type correctly, strugging to follow this late

--- 002_addTwoNumbers/my_code_addTwoNumbers.js
+++ 002_addTwoNumbers/my_code_addTwoNumbers.js
@@ -6,22 +6,52 @@
  * }
  */
 /**
- * @param {ListNode} l1
- * @param {ListNode} l2
- * @return {ListNode}
+//  * @param {ListNode} l1
+//  * @param {ListNode} l2
+//  * @return {ListNode}ß
  */
+
+let a = {
+  val: 2,
+  next : {
+    val : 4,
+    next :{
+      val: 3,
+      next: null
+    }
+  }
+}
+
+let b = {
+  val: 5,
+  next: {
+    val: 6,
+    next: {
+      val: 4,
+      next: null
+    }
+  }
+}
+console.log('a: ', a)
+console.log('b: ', b)
+ 
+
 var addTwoNumbers = function(l1, l2) {
     
-    let check1 = l1.next.val
-    let check2 = l2.next.val
+    let check1 = l1
+    let check2 = l2
     
     let num1 = ''
     let num2 = ''
     
-    while(check1){
-        num1 = check1.val + num1
-        check1 = check1.next
-    }
+    // while(check1.next){
+    //   console.log('while loop check1 list1: ', check1)
+    //   console.log(typeof check1.val.toString())
+    //     num1 = check1.val.toString() + num1;
+    //   console.log('num1: ', num1)
+    //     check1 = check1.next
+
+    // }
     
     while(check2){
         num2 = check2.val + num2
@@ -42,8 +72,10 @@ var addTwoNumbers = function(l1, l2) {
     //             next : splitTotal[i-2] || null
     //          }
     //     }
-    }
+    // }
     
-    return list
+    return list ||'error'
 
 }
+
+console.log(addTwoNumbers(a, b))
