977b740ab319cd58ee95f7e2af9cd50ade5fe1d7
mlsandoval <michaelsandoval8@gmail.com>
2020-01-13 21:36:23 -0800

    think i have solved it, should try solving with recursion though, this solution isnt great.

--- 002_addTwoNumbers/my_code_addTwoNumbers.js
+++ 002_addTwoNumbers/my_code_addTwoNumbers.js
@@ -32,49 +32,57 @@ let b = {
     }
   }
 }
+
 console.log('a: ', a)
 console.log('b: ', b)
- 
 
 var addTwoNumbers = function(l1, l2) {
     
-    let check1 = l1
-    let check2 = l2
-    
-    let num1 = ''
-    let num2 = ''
-    
-    // while(check1.next){
-    //   console.log('while loop check1 list1: ', check1)
-    //   console.log(typeof check1.val.toString())
-    //     num1 = check1.val.toString() + num1;
-    //   console.log('num1: ', num1)
-    //     check1 = check1.next
+  let check1 = l1
+  let check2 = l2
+  
+  let num1 = ''
+  let num2 = ''
+  
+  let count1 = 0
+  while(check1){
+    console.log('check1 while iteration ' + count1 + ' check1 = ', check1)
+    num1 = check1.val + num1
+    console.log('check1 while iteration ' + count1 + ' num1= ' + num1)
+    check1 = check1.next
+    count1++
+  }
 
-    // }
-    
-    while(check2){
-        num2 = check2.val + num2
-        check1 = check2.next
+  let count2 = 0
+  while(check2){
+    console.log('check2 while iteration ' + count2 + ' check2 = ', check2)
+    num2 = check2.val + num2
+    console.log('check2 while iteration ' + count2 + ' num2= ' + num2)
+    check2 = check2.next
+    count2++
+  }
+  
+  let total = parseInt(num1) + parseInt(num2)
+  
+  let splitTotal = total.toString().split('')
+  console.log('splittotal: ', splitTotal)
+  let list
+  
+  let temp = {
+    val: splitTotal[0],
+    next: null
+  }
+  console.log('temp: ', temp)
+
+  for(let i = 1; i < splitTotal.length; i++){
+    list = {
+      val : splitTotal[i],
+      next : temp
     }
-    
-    let total = num1.parseInt() + num2.parseInt()
-    
-    let splitTotal = total.toString().split()
-    console.log(splitTotal)
-    let list
-    
-    // for(let i = splitTotal.length() - 1; i >= 1; i-=2){
-    //     list = {
-    //         val : splitTotal[i],
-    //         next : {
-    //             val : splitTotal[i-1],
-    //             next : splitTotal[i-2] || null
-    //          }
-    //     }
-    // }
-    
-    return list ||'error'
+    temp = list
+  }
+  
+  return list ||'error'
 
 }
 
